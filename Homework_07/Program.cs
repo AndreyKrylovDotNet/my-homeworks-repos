02f2b8c744ff51e2adb56a5e9414780f36e28289
Andrey Krylov <grosstraktor18@gmail.com>
2023-10-29 17:21:45 +0300

    initial

---
+++ Homework_07/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Design;
+using System.IO;
+using System.Text;
+
+namespace Homework_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
